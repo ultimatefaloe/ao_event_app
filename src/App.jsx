ff3b4e37e61393f4a62f52675519601267cf51d3
ultimatefaloe <ultimatefaloe@outlool.com>
2026-08-31 15:21:52 +0100

    project init

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h1 className="text-4xl font-bold text-blue-300">My First Page</h1>
+    </div>
+  );
+}
+
+export default App;
